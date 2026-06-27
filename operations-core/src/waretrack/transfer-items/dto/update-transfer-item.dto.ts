c3a15d7d666161a73897ff85753789251958e454
vincent-guizot <vincentguizot128@gmail.com>
2026-06-28 01:30:21 +0700

    Ware Track Features

---
+++ operations-core/src/waretrack/transfer-items/dto/update-transfer-item.dto.ts
@@ -0,0 +1,5 @@
+import { PartialType } from '@nestjs/swagger';
+
+import { CreateTransferItemDto } from './create-transfer-item.dto';
+
+export class UpdateTransferItemDto extends PartialType(CreateTransferItemDto) {}
